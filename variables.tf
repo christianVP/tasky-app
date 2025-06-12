2dbb3a2418c1434a62128112cd560804f90a83b4
Christian Vendelbo Petersen <christian.v.petersen@gmail.com>
2025-06-12 13:59:52 +0200

    added vars to variables.tf

--- variables.tf
+++ variables.tf
@@ -33,3 +33,24 @@ variable "blob_conn_string" {
   sensitive = true
 }
 
+variable "tenant_id" {
+  description = "The Tenant ID of the Azure Active Directory"
+  type        = string
+}
+
+variable "client_id" {
+  description = "The Client ID of the Service Principal"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Client Secret of the Service Principal"
+  type        = string
+  sensitive   = true  # To mask the secret in logs
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID in Azure"
+  type        = string
+}
+
