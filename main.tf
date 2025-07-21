c94f394c1ec9944782c33eaee492f01b1b3b973d
Christian Vendelbo Petersen <christian.v.petersen@gmail.com>
2025-07-21 12:58:07 +0200

    subject and no dot-meta

--- main.tf
+++ main.tf
@@ -225,10 +225,10 @@ resource "kubernetes_cluster_role_binding" "tasky_cluster_admin" {
     name = "tasky-cluster-admin"
   }
 
-  subjects {
+  subject {
     kind      = "ServiceAccount"
-    name      = kubernetes_service_account.tasky_service_account.metadata[0].name
-    namespace = kubernetes_service_account.tasky_service_account.metadata[0].namespace
+    name      = kubernetes_service_account.tasky_service_account.name
+    namespace = kubernetes_service_account.tasky_service_account.namespace
   }
 
   role_ref {
